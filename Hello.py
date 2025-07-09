bb77e7ad43cfe311ef17923d3503f6a2bb9cb859
Alkihm <alfredkihm@gmail.com>
2025-07-10 00:36:47 +0300

    Initial commit with hello.py

---
+++ Hello.py
@@ -0,0 +1,8 @@
+# hello.py
+
+def say_hello(name):
+    print(f"Hello, {name}! Welcome to your Python learning journey.")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    say_hello(user_name)
